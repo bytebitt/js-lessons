5f2bed1e95b19cc62c26fb06035e73dad5f88cf1
Domestor <Vetal201710@gmail.com>
2025-04-26 15:46:10 +0300

    l3-datatypes

---
+++ js/l3.js
@@ -0,0 +1,28 @@
+// data types
+
+let johnAge = 25;
+console.log(typeof johnAge);
+
+let userName = 'Kayle';
+console.log(typeof userName);
+
+let isMarried = false;
+console.log(typeof isMarried);
+
+let userInfo;
+console.log(typeof userInfo);
+
+let userData = null;
+console.log(typeof userData);
+
+let bigNumber = 12345678901234567890n;
+console.log(typeof bigNumber);
+
+let symbol = Symbol('id');
+console.log(typeof symbol);
+
+let objectType = {
+    'name': 'John',
+    'age': 25
+};
+console.log(typeof objectType);
